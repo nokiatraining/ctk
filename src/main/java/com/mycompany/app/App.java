f727eb1d33f81a839c8d96282fa2b289a1da9512
C T <kruthi.c_t@on.nokia.com>
2017-03-19 12:08:19 +0530

    first commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+		String test ="test";
         System.out.println( "Hello World!" );
 
         System.out.println("A NullPointerException is a drag...");
